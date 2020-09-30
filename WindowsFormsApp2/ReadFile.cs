0e8591cecdbb906a0c0de1ab15cc3f535a6945d3
Thapelo Tlhako <576525@student.belgiumcampus.ac.za>
2020-09-30 17:11:28 +0200

    adding file handler and Camp classes

---
+++ WindowsFormsApp2/ReadFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class ReadFile
+    {
+        public List<string> Read(string fileName)
+        {
+            List<string> data = new List<string>();
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fs);
+
+            string txt = string.Empty;
+            while ((txt = reader.ReadLine()) != null)
+            {
+                data.Add(txt);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ WindowsFormsApp2/WriteFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class WriteFile
+    {
+        public void Write(string fileName, List<string> data)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs);
+            foreach (var item in data)
+            {
+                writer.WriteLine(item);
+            }
+        }
+    }
+}
